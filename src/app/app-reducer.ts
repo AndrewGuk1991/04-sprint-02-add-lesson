934c126fc0517707d1247942e4bf94ccadec7e7c
andrew_guk <gukandrey91@gmail.com>
2026-06-05 19:37:51 +0300

    add error handling in updateDeckTC

--- src/app/app-reducer.ts
+++ src/app/app-reducer.ts
@@ -24,3 +24,4 @@ export const changeAppStatusAC = (status: RequestStatusType) => {
     payload: status
   } as const
 }
+
--- src/features/decks/decks-thunks.ts
+++ src/features/decks/decks-thunks.ts
@@ -2,6 +2,7 @@ import { Dispatch } from 'redux'
 import { decksAPI, UpdateDeckParams } from './decks-api.ts'
 import { addDeckAC, deleteDeckAC, setDecksAC, updateDeckAC } from './decks-reducer.ts'
 import { changeAppStatusAC } from '../../app/app-reducer.ts'
+import { isAxiosError } from 'axios'
 
 export const fetchDecksTC = () => async (dispatch: Dispatch) => {
   try {
@@ -28,7 +29,16 @@ export const deleteDeckTC = (id: string) => async (dispatch: Dispatch) => {
 }
 
 export const updateDeckTC = (params: UpdateDeckParams) => async (dispatch: Dispatch) => {
-  return decksAPI.updateDeck(params).then((res) => {
+  try {
+    const res = await decksAPI.updateDeck(params)
     dispatch(updateDeckAC(res.data))
-  })
+  } catch (e) {
+    let errorMessage
+    if (isAxiosError(e)) {
+      errorMessage = e.response?.data?.errorMessages[0].message || e.message
+    } else if (e instanceof Error) {
+      errorMessage = e.message
+    }
+    console.log(errorMessage)
+  }
 }
